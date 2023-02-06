754580cf7e5f7b1a44caece306c589997df8b04a
germaniko <german.m.pesce@gmail.com>
2023-02-06 20:16:31 -0300

    add byCountry component html and Observable to searchCuuntry in the api Rest

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
 })
-export class ByCountryComponent implements OnInit {
+export class ByCountryComponent {
+  term: string = 'Hola Mundo';
 
-  constructor() { }
+  constructor(private countryService: CountryService) {}
 
-  ngOnInit(): void {
+  search() {
+    this.countryService.searchCountry(this.term).subscribe((resp) => {
+      console.log(resp);
+    });
   }
-
 }
